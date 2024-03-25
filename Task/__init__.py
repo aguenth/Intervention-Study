3c72cc542cc279463993fe98fd4d321e7b7a25aa
aguenth <anne.guenther@unibas.ch>
2024-03-25 09:18:50 +0100

    rm db

--- Task/__init__.py
+++ Task/__init__.py
@@ -2,18 +2,15 @@ from otree.api import *
 import random
 import os
 
-
-from attributes_en import attributes_listA_small, attributes_listA_medium, attributes_listA_large, attributes_listB_small, attributes_listB_medium, attributes_listB_large, attributes_listC_small, attributes_listC_medium, attributes_listC_large, attributes_listD_small, attributes_listD_medium, attributes_listD_large, attributes_listE
-from attributes_de import attributes_listA_small_de, attributes_listA_medium_de, attributes_listA_large_de, attributes_listB_small_de, attributes_listB_medium_de, attributes_listB_large_de, attributes_listC_small_de, attributes_listC_medium_de, attributes_listC_large_de, attributes_listD_small_de, attributes_listD_medium_de, attributes_listD_large_de, attributes_listE_de
-from attributes_es import attributes_listA_small_es, attributes_listA_medium_es, attributes_listA_large_es, attributes_listB_small_es, attributes_listB_medium_es, attributes_listB_large_es, attributes_listC_small_es, attributes_listC_medium_es, attributes_listC_large_es, attributes_listD_small_es, attributes_listD_medium_es, attributes_listD_large_es, attributes_listE_es
-
-print(os.getcwd())
+# Change directory to the correct location
+os.chdir('/Users/aguenther/Documents/GitHub/Intervention-Study/')
 
 author = 'Anne Guenther'
 
 doc = """
 Mouse Tracking Task
 """
+
 
 # Constants
 class Constants(BaseConstants):
@@ -22,6 +19,8 @@ class Constants(BaseConstants):
     blocks = ['A', 'B', 'C', 'D', 'E']
     trials_per_block = 18
     num_rounds = 90
+
+    attributes_listA_small = open('Intervention-Study/attributes_en.py')
 
     # Add block-specific texts
     block_texts = {
