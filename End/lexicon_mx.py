f9e2ee2f11491e62256f3f4fe9f8ca717f43ed38
aguenth <anne.guenther@unibas.ch>
2024-04-15 17:08:58 +0200

    task lexicon spanish

--- End/lexicon_mx.py
+++ End/lexicon_mx.py
@@ -1,5 +1,6 @@
 class Lexicon:
-    end_title = 'Fin'
-    thanks = "Gracias por su participación."
-    redirect_instructions = "PLEASE CLICK BELOW TO FINISH AND SUBMIT YOUR RESULTS"
-    finish = "Pulse aquí para terminar"
+
+    end_title = "Fin"
+    thanks = "Gracias por tu participación."
+    redirect_instructions = "HAZ CLIC A CONTINUACIÓN PARA TERMINAR Y ENVIAR TUS RESULTADOS"
+    finish = "Haz clic aquí para terminar"
--- Questionnaires/lexicon_mx.py
+++ Questionnaires/lexicon_mx.py
@@ -1,3 +1,90 @@
 class Lexicon:
+    instructions = "Instrucciones"
 
-    start="Start"
+    next = "Siguiente"
+
+    no = "No"
+
+    yes = "Sí"
+
+    # Scales = NA NA
+    not_at_all = "para nada"
+
+    very_much = "muy en desacuerdo"
+
+    completely_disagree = "totalmente en desacuerdo"
+
+    completely_agree = "totalmente de acuerdo"
+
+    unlikely = "muy improbable"
+
+    likely = "muy probable"
+
+    # Transition
+    transition_title = 'Bienvenido al último bloque del estudio'
+
+    transition_thanks = '¡Gracias por evaluar las ofertas de vehículos eléctricos y los paquetes de políticas!'
+
+    transition_info = 'En las siguientes páginas, te haremos algunas preguntas más. Por favor, haz clic en "siguiente" para continuar'
+
+    # Subjective Probability
+    probability_title = 'Ventajas y desventajas de los vehículos eléctricos'
+
+    intro_probability = 'A continuación encontrarás algunas preguntas generales sobre las ventajas e inconvenientes de los vehículos eléctricos para ti personalmente'.NA
+    question_advantages = '¿Qué probabilidad crees que tienes de beneficiarte de las siguientes ventajas al comprar un vehículo eléctrico?'
+
+    answer_intro_advantages = 'Comprar un vehículo eléctrico me permitiría...'
+    question_disadvantages = '¿Qué probabilidad crees que tienes de experimentar los siguientes inconvenientes al comprar un vehículo eléctrico?'
+
+    answer_intro_disadvantages = 'Comprar un vehículo eléctrico supondría...'
+
+    ev_prob_benefits1_label = '...reducir considerablemente mis gastos.'
+
+    ev_prob_benefits2_label = "...aumentar considerablemente mi independencia"
+    ev_prob_benefits3_label = '...disminuir considerablemente mi impacto sobre el clima'
+    ev_prob_benefits4_label = '...formar parte de un objetivo o acción común en la que también participan otros.'
+
+    ev_prob_risks1_label = '...costos iniciales demasiado elevados.'
+
+    ev_prob_risks2_label = '...un rendimiento de la inversión demasiado bajo.'
+
+    # Perceived Risk and benefit = NA NA
+    risks_title = "Tu percepción de los riesgos y beneficios asociados a los vehículos eléctricos"
+
+    risk_agree = "¿Hasta qué punto estás de acuerdo con las siguientes afirmaciones?"
+
+    ev_perceived_risk1_label = "Comprar un vehículo eléctrico es un riesgo para mí"
+    ev_perceived_risk2_label = "Creo que comprar un vehículo eléctrico conlleva demasiados riesgos"
+    ev_perceived_benefit1_label = "Comprar un vehículo eléctrico me resulta atractivo"
+    ev_perceived_benefit2_label = "Creo que comprar un vehículo eléctrico tiene muchas ventajas"
+    # Climate Change Concern (Tobler et al., 2012 & Shi et al., 2016)
+    cc_concern_title = "Preocupación por el Cambio Climático"
+
+    cc_concern_intro = "¿Hasta qué punto estás de acuerdo con las siguientes afirmaciones?"
+
+    cc_concern1_label = "Me preocupa que el clima esté cambiando"
+    cc_concern2_label = "La protección del clima es importante para nuestro futuro"
+    cc_concern3_label = "Debemos proteger el equilibrio del clima"
+    cc_concern4_label = "El cambio climático tiene graves consecuencias para los seres humanos y la naturaleza"
+    # Political Orientation = NA NA
+    pol_orientation_title = "Orientación Política"
+
+    pol_orientation_text = "<b>Liberal</b> y <b>Conservador</b> son términos utilizados habitualmente para caracterizar la orientación política. Por favor, indica en la siguiente escala que va de 1 (extremadamente liberal) a 10 (extremadamente conservador) cómo te clasificarías en términos de orientación política."
+
+    strongly_liberal = "Muy liberal"
+    moderate = "Moderado"
+    strongly_conservative = "Muy conservador"
+    # Neighborhood
+    not_similar = "muy diferente"
+    very_similar = "muy similar"
+    neighborhood_title = "Preguntas de Comunicación"
+    wom_talking_label = "¿Con cuántas personas hablas de coches?<br> Piensa en tus amigos, vecinos, colegas. Introduce el número aproximado de personas."
+    homophily_intro = "Cuando piensas en estas personas, ¿en qué se parecen a ti en términos de..."
+    homophily1_label = "...la región en la que viven (urbana, suburbana, rural)?"
+    homophily2_label = "...su educación?"
+    homophily3_label = "...sus ingresos?"
+    homophily4_label = "...sus opiniones políticas?"
+
+    # Comments = NA NA
+    comments_title = "¿Tienes algún comentario antes de terminar el estudio?"
+    comments_instruction = "Escribe tus comentarios en la casilla o deja esta casilla en blanco"
