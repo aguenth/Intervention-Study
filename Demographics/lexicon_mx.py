3da6b913671d75e0fd5183c0cc4661e69114fd9e
aguenth <anne.guenther@unibas.ch>
2024-04-23 16:16:30 +0200

    income mx

--- Demographics/lexicon_mx.py
+++ Demographics/lexicon_mx.py
@@ -32,11 +32,11 @@ class Lexicon:
     doctor = "Doctorado"
 
     income_label = "¿A cuánto ascienden los ingresos anuales de tu <b>hogar</b> antes de pagar impuestos?"
-    income_quintile1 = "< 200,000 pesos"
-    income_quintile2 = "200,000 a 300,000 pesos"
-    income_quintile3 = "300,000 a 400,000 pesos"
-    income_quintile4 = "400,000 a 500,000 pesos"
-    income_quintile5 = ">500,000 pesos"
+    income_quintile1 = "< 30,000 pesos"
+    income_quintile2 = "30,001 a 75,000 pesos"
+    income_quintile3 = "75,001 a 120,000 pesos"
+    income_quintile4 = "120,001 a 400,000 pesos"
+    income_quintile5 = ">400,000 pesos"
 
     household_label = "¿Cuántas personas viven en tu hogar?"
 
